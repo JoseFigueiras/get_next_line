0067f1d56842c2a807ad6a227e7db1a2b1a4dc25
Josefig <josefiggaming@gmail.com>
2021-03-13 14:11:03 +0000

    remove define buffer_size

--- get_next_line.h
+++ get_next_line.h
@@ -4,10 +4,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE = 2048
-# endif
-
 int		get_next_line(int fd, char **line);
 int		has_nl(char *str);
 char	*join_str(const char *str1, const char *str2);
